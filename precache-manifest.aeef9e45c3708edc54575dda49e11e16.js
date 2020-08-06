7208c222fae1363cab387ce61c95097112ea82b2
Roberth Silva <roberthvipleiloes@hotmail.com>
2020-08-06 20:15:31 -0300

    Updates

---
+++ precache-manifest.aeef9e45c3708edc54575dda49e11e16.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "607a08b3372823979902335855fe6b31",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "6824d568d7f38cf0ddd3",
+    "url": "/portfolio/static/js/2.49a72d03.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.49a72d03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d8e6aff60672a4460f7",
+    "url": "/portfolio/static/js/main.5173609f.chunk.js"
+  },
+  {
+    "revision": "9dd9ca3ab571d95c6170",
+    "url": "/portfolio/static/js/runtime-main.ad451841.js"
+  }
+]);
